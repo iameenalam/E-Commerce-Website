5c3ad78c2213c273bc0607335f622555738a84ab
iameenalam <ameenalam98@gmail.com>
2025-06-28 13:15:50 +0500

    removed development mode

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -31,9 +31,5 @@ export async function GET() {
     });
   }
 
-  return NextResponse.redirect(
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/"
-      : "https://solezaar.vercel.app/"
-  );
+  return NextResponse.redirect("https://solezaar.vercel.app/");
 }
